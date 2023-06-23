bbcc9982e64c2b502de4d6a9f4ff3f7ea67410d3
Zuzana Benikova <benikovaz@gmail.com>
2023-06-23 12:09:53 +0200

    Prve pridanie do skriptu + vyskusanie novej vetvy.

--- First script.py
+++ First script.py
@@ -1,2 +1,18 @@
 print('Hello_World!')
 
+
+
+stastna = input("Si šťastná?")
+bohata = input("Si bohatá?")
+
+if stastna == "ano"
+    if bohata == "ano":
+        print("Gratulujem!")
+    else:
+        print(Skus menej utrácať)
+else:
+    if bohata == "ano"
+        print("Skus sa viac usmievať.")
+    else:
+        print("To je mi ľúto.")
+
