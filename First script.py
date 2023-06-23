c6ed5e0a173ba0569f0485aa4c08f9a46925757b
Zuzana Benikova <benikovaz@gmail.com>
2023-06-23 13:13:07 +0200

    Oprava skriptu.

--- First script.py
+++ First script.py
@@ -6,14 +6,13 @@ print('Hello_World!')
 stastna = input("Si šťastná?")
 bohata = input("Si bohatá?")
 
-if stastna == "ano"
+if stastna == "ano":
     if bohata == "ano":
         print("Gratulujem!")
     else:
         print("Skus menej utrácať")
 else:
-    if bohata == "ano"
+    if bohata == "ano":
         print("Skus sa viac usmievať.")
     else:
         print("To je mi ľúto.")
-
