8223973ccd3d0bd1d73c7da9231d6158589dacd1
Zuzana Benikova <benikovaz@gmail.com>
2023-07-11 10:54:50 +0200

    Doplnenie retazca.

--- First script.py
+++ First script.py
@@ -7,19 +7,17 @@ if stastna_retezec == 'ano' or stastna_retezec == 'Ano':
 elif stastna_retezec == 'ne' or stastna_retezec == 'Ne' or stastna_retezec == 'Nie' or stastna_retezec == 'nie':
     stastna = False
 else:
-    print('Nerozumiem!')
-
-# Doplnit smycku pri nerozumiem, nech sa opyta este raz. !
+    print('Nerozumiem!')            # Doplnit smycku pri nerozumiem, nech sa opyta este raz.
 
 bohata_retezec = input('Si bohatá? ')
 if bohata_retezec == 'ano' or bohata_retezec == 'Ano':
-    bohata = True
+    bohata_retezec = True
 elif bohata_retezec == 'ne' or bohata_retezec == 'Ne' or bohata_retezec == 'Nie' or bohata_retezec == 'nie':
-    bohata = False
+    bohata_retezec = False
 else:
     print('Nerozumiem!')
 
-if bohata and stastna:
+if  bohata_retezec and stastna:
     # Je bohatá a zároveň štǎstná, ta sa má.
     print('Gratulujem!')
 elif bohata:
